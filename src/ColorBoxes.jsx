09771f151316940fc5b5020f63c9a0e2f8065f56
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-06 14:18:41 +0200

    Add Colors Array

--- src/ColorBoxes.jsx
+++ src/ColorBoxes.jsx
@@ -2,7 +2,25 @@ import ColorBox from "./ColorBox"
 export default function ColorBoxes() {
   return (
     <div>
-        <ColorBox/>
+        <ColorBox colors={
+            [
+                "#E53935",
+    "#E91E63",
+    "#9C27B0",
+    "#673AB7",
+    "#3F51BF",
+    "#2196F3",
+    "#03A9F4",
+    "#00BCD4",
+    "#009688",
+    "#4CAF50",
+    "#8BC34A",
+    "#CDDC39",
+    "#FFEB3B",
+    "#FF9800",
+    "#FF5722",
+            ]
+        }/>
     </div>
   )
 }
