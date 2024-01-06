4cb93c9b60a1e8489d4b5bc85abc0006a2a821a9
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-06 15:28:15 +0200

    Make ColorBox Generate 25 squares with random colors and a click event to change color

--- src/ColorBox.jsx
+++ src/ColorBox.jsx
@@ -2,15 +2,21 @@ import {useState} from 'react';
 export default function ColorBox({colors}) {
 
   const RandColor = ()=> colors[Math.floor(Math.random() * colors.length)];
-  const [color, setColor] = useState(RandColor());
-  const [styles, setStyles] = useState({backgroundColor: color, width: '100px', height:'100px'});
-
-  function ChangeColor(){
-    setColor(RandColor());
-    setStyles({backgroundColor: color, width: '100px', height:'100px'});
-  }
     
   return (
-    <div style={styles} onClick={ChangeColor}></div>
+      colors.map((b, index) =>{
+      const [color, setColor] = useState(RandColor());
+      const [styles, setStyles] = useState({backgroundColor: color, width: '100px', height:'100px'});
+      
+      function ChangeColor(){
+        const newColor = RandColor();
+        setColor(newColor);
+        setStyles({backgroundColor: newColor, width: '100px', height:'100px'});
+      }
+
+      return (
+        <div key={index} style={styles} onClick={ChangeColor}></div>
+      )
+    })
   )
 }
