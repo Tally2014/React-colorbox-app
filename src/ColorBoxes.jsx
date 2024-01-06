36f0ad5fb7b93e695be6c8e03d4a527f5ab2f2d3
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-06 15:40:51 +0200

    Styling the ColorBoxes to a Grid

--- src/ColorBoxes.jsx
+++ src/ColorBoxes.jsx
@@ -1,24 +1,25 @@
 import ColorBox from "./ColorBox"
+import "./ColorBoxes.css"
 export default function ColorBoxes() {
   return (
-    <div>
+    <div className="Colorboxes">
         <ColorBox colors={
             [
                 "#E53935",
-    "#E91E63",
-    "#9C27B0",
-    "#673AB7",
-    "#3F51BF",
-    "#2196F3",
-    "#03A9F4",
-    "#00BCD4",
-    "#009688",
-    "#4CAF50",
-    "#8BC34A",
-    "#CDDC39",
-    "#FFEB3B",
-    "#FF9800",
-    "#FF5722",
+                "#E91E63",
+                "#9C27B0",
+                "#673AB7",
+                "#3F51BF",
+                "#2196F3",
+                "#03A9F4",
+                "#00BCD4",
+                "#009688",
+                "#4CAF50",
+                "#8BC34A",
+                "#CDDC39",
+                "#FFEB3B",
+                "#FF9800",
+                "#FF5722",
             ]
         }/>
     </div>
